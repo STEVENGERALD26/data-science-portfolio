47357530765a10fe898560bccbca546ff769ecd6
STEVENGERALD26 <112558745+STEVENGERALD26@users.noreply.github.com>
2026-09-10 20:58:38 -0700

    Add favorite subject question and extra welcome messages

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,8 +1,11 @@
-   # My first Python project in GitHub
-   
-   def greet_user():
-       name = input("Enter your name: ")
-       print(f"Hello, {name}! Welcome to Data Science.")
-   
-   if __name__ == "__main__":
-       greet_user()
+# My first Python project in GitHub
+
+def greet_user():
+    name = input("Enter your name: ")
+    subject = input("What is your favorite subject? ")
+    print(f"Hello, {name}! Welcome to Data Science.")
+    print(f"It's awesome that you enjoy {subject}!")
+    print("You are going to do great things!")
+
+if __name__ == "__main__":
+    greet_user()
